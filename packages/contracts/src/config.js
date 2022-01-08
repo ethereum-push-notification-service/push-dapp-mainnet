e557fd07d524ae0a25eb4c694ada0ef64728bb59
xander <shuaibualexander@gmail.com>
2022-01-09 00:57:06 +0100

    update RPC METHOD

--- packages/contracts/src/config.js
+++ packages/contracts/src/config.js
@@ -6,7 +6,7 @@ const config = {
         137 //for polygon
     ],
     coreContractChain: 1, //the chain id of the network which the core contract relies on
-    coreRPC: "https://mainnet.infura.io/v3/TZCWZ8YCQDH4THP54865SDGTG3XXY8ZAQU"
+    coreRPC: "https://mainnet.infura.io/v3/4ff53a5254144d988a8318210b56f47a"
 };
 
 export default config;
